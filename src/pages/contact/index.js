0d596eff3671c28baedd641da4539a32d7f2b26d
ayush12993 <ayush2202ksr@gmail.com>
2024-04-06 02:06:23 +0530

    email corrected

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -96,13 +96,15 @@ const [formData, setFormData] = useState({
                         <input name="name" type="text" placeholder="Ayush Jain" value={formData.name} onChange={handleChange} className="input-collapsible"/>
                         <input name="email" type="email" placeholder="ayush2202ksr@gmail.com" value={formData.email} onChange={handleChange} className="input-collapsible"/>
                         <textarea name="message" type="text" placeholder="Enter your message here"  value={formData.message} onChange={handleChange} className="input-collapsible"/>
-                        <button className="not-big-screen-text"  href="javascript:;" onclick="document.getElementById('emailSubmit').submit();">
+                      <button type="submit">
+  <a className="not-big-screen-text"  href="javascript:;" onclick="document.getElementById('emailSubmit').submit();">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-corner-down-right" viewBox="0 0 24 24">
                                 <path d="M15 10l5 5-5 5" />
                                 <path d="M4 4v7a4 4 0 004 4h12" />
                             </svg>
-                     ___________
-                        </button>
+                     Contact now
+                        </a>
+                      </button>
                         </form>
                     </div>
                 </div>
