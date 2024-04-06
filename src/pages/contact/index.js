7e8784db59cbfdec24502422c08d4d66245377c1
ayush12993 <ayush2202ksr@gmail.com>
2024-04-07 01:09:56 +0530

    second commit of it

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -101,11 +101,8 @@ function emailing(){
                         <input name="email" type="email" placeholder="Your email" value={formData.email} onChange={handleChange} className="input-collapsible"/>
                         <textarea name="message" type="text" placeholder="Enter your project idea"  value={formData.message} onChange={handleChange} className="input-collapsible"/>
                         <a className="not-big-screen-text"  href="javascript:;" onclick="emailing()">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-corner-down-right" viewBox="0 0 24 24">
-                                <path d="M15 10l5 5-5 5" />
-                                <path d="M4 4v7a4 4 0 004 4h12" />
-                            </svg>
-                     Contact me
+                           
+                     <button className="buttonSize" type="submit" >Submit now</button>
                         </a>
                         </form>
                     </div>
